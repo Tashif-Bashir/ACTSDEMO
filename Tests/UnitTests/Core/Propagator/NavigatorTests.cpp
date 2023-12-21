83945351383235a58707d912f35ec8b670b6b322
andiwand <stefl.andreas@gmail.com>
2023-12-21 12:11:06 +0100

    add try all overstep navigator

--- Tests/UnitTests/Core/Propagator/NavigatorTests.cpp
+++ Tests/UnitTests/Core/Propagator/NavigatorTests.cpp
@@ -29,6 +29,7 @@
 #include "Acts/Propagator/StraightLineStepper.hpp"
 #include "Acts/Propagator/SurfaceCollector.hpp"
 #include "Acts/Propagator/TryAllNavigator.hpp"
+#include "Acts/Propagator/TryAllOverstepNavigator.hpp"
 #include "Acts/Propagator/detail/SteppingHelper.hpp"
 #include "Acts/Surfaces/BoundaryCheck.hpp"
 #include "Acts/Surfaces/Surface.hpp"
@@ -1055,6 +1056,10 @@ using StraightLinePropagator = Propagator<StraightLineStepper, Navigator>;
 using Reference1EigenPropagator = Propagator<EigenStepper, TryAllNavigator>;
 using Reference1StraightLinePropagator =
     Propagator<StraightLineStepper, TryAllNavigator>;
+using Reference2EigenPropagator =
+    Propagator<EigenStepper, TryAllOverstepNavigator>;
+using Reference2StraightLinePropagator =
+    Propagator<StraightLineStepper, TryAllOverstepNavigator>;
 
 EigenStepper estepper(bField);
 StraightLineStepper slstepper;
@@ -1080,6 +1085,18 @@ Reference1StraightLinePropagator refslpropagator1(
     TryAllNavigator({tGeometry, true, true, false},
                     getDefaultLogger("ref1_sl_nav", Logging::INFO)),
     getDefaultLogger("ref1_sl_prop", Logging::INFO));
+
+Reference2EigenPropagator refepropagator2(
+    estepper,
+    TryAllOverstepNavigator({tGeometry, true, true, false,
+                             BoundaryCheck(false)},
+                            getDefaultLogger("ref2_e_nav", Logging::INFO)),
+    getDefaultLogger("ref2_e_prop", Logging::INFO));
+Reference2StraightLinePropagator refslpropagator2(
+    slstepper,
+    TryAllOverstepNavigator({tGeometry, true, true, false},
+                            getDefaultLogger("ref2_sl_nav", Logging::INFO)),
+    getDefaultLogger("ref2_sl_prop", Logging::INFO));
 
 BOOST_DATA_TEST_CASE(
     Navigator_random,
@@ -1123,6 +1140,24 @@ BOOST_DATA_TEST_CASE(
     std::cout << ">>> Test self consistency slpropagator" << std::endl;
   }
   runSelfConsistencyTest(slpropagator, start, debugMode);
+
+  if (debugMode) {
+    std::cout << ">>> Test reference 1 consistency epropagator" << std::endl;
+  }
+  runConsistencyTest(epropagator, refepropagator1, start, debugMode);
+  if (debugMode) {
+    std::cout << ">>> Test reference 1 consistency slpropagator" << std::endl;
+  }
+  runConsistencyTest(slpropagator, refslpropagator1, start, debugMode);
+
+  if (debugMode) {
+    std::cout << ">>> Test reference 2 consistency epropagator" << std::endl;
+  }
+  runConsistencyTest(epropagator, refepropagator2, start, debugMode);
+  if (debugMode) {
+    std::cout << ">>> Test reference 2 consistency slpropagator" << std::endl;
+  }
+  runConsistencyTest(slpropagator, refslpropagator2, start, debugMode);
 }
 
 }  // namespace Test
