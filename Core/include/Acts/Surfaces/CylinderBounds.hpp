0fa27a573c7e3c9ead2df99a9cb18f5fdb2d5ad4
Andreas Stefl <stefl.andreas@gmail.com>
2024-05-22 11:39:19 +0200

    reduce `CylinderBounds` diff

--- Core/include/Acts/Surfaces/CylinderBounds.hpp
+++ Core/include/Acts/Surfaces/CylinderBounds.hpp
@@ -10,7 +10,9 @@
 
 #include "Acts/Definitions/Algebra.hpp"
 #include "Acts/Definitions/Tolerance.hpp"
+#include "Acts/Surfaces/BoundaryCheck.hpp"
 #include "Acts/Surfaces/SurfaceBounds.hpp"
+#include "Acts/Utilities/detail/periodic.hpp"
 
 #include <array>
 #include <cmath>
@@ -138,6 +140,35 @@ class CylinderBounds : public SurfaceBounds {
   ActsMatrix<2, 2> jacobian() const;
 };
 
-}  // namespace Acts
+inline std::vector<double> CylinderBounds::values() const {
+  std::vector<double> valvector;
+  valvector.insert(valvector.begin(), m_values.begin(), m_values.end());
+  return valvector;
+}
 
-#include "Acts/Surfaces/CylinderBounds.ipp"
+inline bool CylinderBounds::coversFullAzimuth() const {
+  return m_closed;
+}
+
+inline void CylinderBounds::checkConsistency() noexcept(false) {
+  if (get(eR) <= 0.) {
+    throw std::invalid_argument("CylinderBounds: invalid radial setup.");
+  }
+  if (get(eHalfLengthZ) <= 0.) {
+    throw std::invalid_argument("CylinderBounds: invalid length setup.");
+  }
+  if (get(eHalfPhiSector) <= 0. || get(eHalfPhiSector) > M_PI) {
+    throw std::invalid_argument("CylinderBounds: invalid phi sector setup.");
+  }
+  if (get(eAveragePhi) != detail::radian_sym(get(eAveragePhi))) {
+    throw std::invalid_argument("CylinderBounds: invalid phi positioning.");
+  }
+  if (get(eBevelMinZ) != detail::radian_sym(get(eBevelMinZ))) {
+    throw std::invalid_argument("CylinderBounds: invalid bevel at min Z.");
+  }
+  if (get(eBevelMaxZ) != detail::radian_sym(get(eBevelMaxZ))) {
+    throw std::invalid_argument("CylinderBounds: invalid bevel at max Z.");
+  }
+}
+
+}  // namespace Acts
